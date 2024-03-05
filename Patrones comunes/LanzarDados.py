face71dd5b9ee02cd13c63ce04449c8261a0f559
ealexandergarcia <egarciapame@gmail.com>
2024-03-05 18:11:05 -0500

    ninth common pattern exercise

---
+++ Patrones comunes/LanzarDados.py
@@ -0,0 +1,5 @@
+# Escriba un programa que muestre todas las combinaciones posibles al momento de lanzar dos dados de 6 caras:
+for i in range(1,7):
+    for j in range(1,7):
+        print (i,j)
+    print()
